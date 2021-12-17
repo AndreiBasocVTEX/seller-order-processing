3bdcf648777c04756821476161acd45fb74aabc6
arotari <annaxrotari@gmail.com>
2021-12-17 14:29:04 +0200

    Get orders list

--- react/sellerDashboard.tsx
+++ react/sellerDashboard.tsx
@@ -1,6 +1,25 @@
+import React, { useState, useCallback } from 'react'
 import type { FC } from 'react'
-import React from 'react'
+import axios from 'axios'
 
-const SellerDashboard: FC = () => <div>It works!</div>
+const SellerDashboard: FC = () => {
+  const [orderList, setOrderList] = useState([])
+
+  const getOrderList = useCallback(async () => {
+    try {
+      const { data } = await axios.get(`/api/oms/pvt/orders?f_creationdate`, {
+        headers: { Accept: 'application/json' },
+      })
+
+      setOrderList(data)
+    } catch (e) {
+      console.log(e)
+    }
+  }, [])
+
+  console.log(orderList)
+
+  return <button onClick={getOrderList}>Click Me</button>
+}
 
 export default SellerDashboard
