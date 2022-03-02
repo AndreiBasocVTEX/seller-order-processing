f7e3cf6633aca8f2414d6621348b1d015adc8731
Vlad Morari <vmorari@fusionworks.md>
2022-03-02 12:58:05 +0000

    Revert "Merge branch 'FSWK-310' into 'staging'"
    This reverts merge request !125

--- react/src/pages/OrdersList/index.tsx
+++ react/src/pages/OrdersList/index.tsx
@@ -374,14 +374,6 @@ const OrdersList: FC = () => {
     if (search || status || date) {
       setTableLoading(false)
     }
-
-    if (tableItems?.length < paginationParams.perPage) {
-      setPaginationParams({
-        ...paginationParams,
-        page: 1,
-        itemsFrom: 1,
-      })
-    }
   }, [tableItems])
 
   useEffect(() => {
